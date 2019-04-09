d4ad59b7ed003af5ed030e3bc43e3d62672b7044
DESKTOP-95TJ6IL\Aji Suprana <ajiadiwenas@gmail.com>
2019-04-09 16:17:00 +0700

    :

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 const userLogin = require('./userlogin/userlogin');
+const errorHandler = require("./errorHandler/ErrorHandler")
 
 userLogin.initialize(app);
+errorHandler.initialize(app);
+
+mongoose.connect(
+    //"mongodb+srv://quest_god:QuestGod1@questgod-ftvdm.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://quest_god:"+
+    process.env.MONGO_ATLAS_PW+
+    "@questgod-ftvdm.mongodb.net/test?retryWrites=true",
+    {
+       useNewUrlParser: true
+    }
+);
+
+//mongodb+srv://quest_god:<password>@questgod-ftvdm.mongodb.net/test?retryWrites=true
 
 module.exports = app;
-
---
+++ errorHandler/ErrorHandler.js
@@ -0,0 +1,41 @@
+
+class ErrorHandler {
+    initialize(expressApp) {
+        this.express = expressApp;
+        console.log('ErrorHandler class is initialized');
+        this.RoutesHandler();
+    }
+
+    RoutesHandler() {
+        //Prevent CORS ERROR
+        this.express.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers',
+                'Origin,X-Requested-With, Content-Type,Accept,Authorization'
+            );
+            if (req.method === 'OPTIONS') {
+                res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+                return res.status(200).json({});
+            }
+            next();
+        });
+        //Handling all Errors
+        this.express.use((req, res, next) => {
+            const error = new Error('Not found');
+            error.status = 404;
+            next(error);
+        })
+
+        this.express.use((error, req, res, next) => {
+            res.status(error.status || 500);
+            res.json({
+                error: {
+                    message: error.message
+                }
+            });
+        })
+
+    }
+}
+
+module.exports = new ErrorHandler();
